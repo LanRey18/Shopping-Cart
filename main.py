ae84e8b334fe93a1b55dbffa6818d26ca03d3065
Graham Russell Gonzales <graham.a.gonzales@gmail.com>
2025-02-01 12:14:19 +0800

    Finished

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
-from fastapi import FastAPI, Request, Form
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel  
+from pydantic.dataclasses import dataclass
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 items = [
-            {"prodId": 1, "name": "Apple", "quantity" : 5, "price" : 22},
-            {"prodId": 2, "name": "Orange", "quantity" : 5, "price" : 22},
-            {"prodId": 3, "name": "Grapes", "quantity" : 5, "price" : 22},
-            {"prodId": 4, "name": "Grapes", "quantity" : 5, "price" : 22},
+            {"prodId": 1, "name": "Sunscreen", "quantity" : 100, "price" : 10.99},
+            {"prodId": 2, "name": "Facial Cleanser", "quantity" : 100, "price" : 15.59},
+            {"prodId": 3, "name": "Niacininamide Serum", "quantity" : 100, "price" : 20},
+            {"prodId": 4, "name": "Hyaluronic acid", "quantity" : 100, "price" :29.99},
+            {"prodId": 5, "name": "Moisturizer", "quantity" : 100, "price" : 15.99},
+            {"prodId": 6, "name": "Vitamin C Serum", "quantity" : 100, "price" : 15},
+            {"prodId": 7, "name": "Toner", "quantity" : 100, "price" : 9.99},
+            {"prodId": 8, "name": "Retinol", "quantity" : 100, "price" : 24.99},
         ]
 
-orders = [
-            {"prodId": 1, "name": "Apple", "quantity" : 2, "price" : 22},
-         ]
+orders = []
+
 
 class Item(BaseModel):
-    prodId: int = None
-    name: str = None
-    quantity: int = None
-    price: int = None
+    prodId: int 
+    name: str 
+    quantity: int 
+    price: int 
 
 class OrderRequest(BaseModel):
-    prodId: int
+    prodId: int 
     quantity: int 
 
 @app.get("/", response_class=HTMLResponse)
@@ -32,10 +37,10 @@ async def home(request: Request):
     return templates.TemplateResponse("Home.html", {"request": request})
 
 @app.get("/items")
-def get_items(request: Request):
+async def get_items(request: Request ):
     return templates.TemplateResponse("ViewItems.html", {"request": request,  "items": items})
 
-@app.post("/items")
+@app.post("/create_item")
 def add_item(item : Item):
     items.append(item)
     return items
@@ -66,7 +71,8 @@ def order_item(order_request: OrderRequest):
                     "price": item["price"] * quantity
                 }
                 orders.append(ordered_item)
-                return {"message": "Order successful", "order": ordered_item}
+                return RedirectResponse("/", status_code=303) 
+            # {"message": "Order successful", "orders": orders, "items" : items}
             else:
                 return {"error": "Insufficient quantity available"}
 
